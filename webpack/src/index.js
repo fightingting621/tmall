542b8ff817a0a9035ea50ee0df25a0969245d2f8
Dell <qwertxz>
2018-08-07 10:55:59 +0800

    webpack打包学习

---
+++ webpack/src/index.js
@@ -0,0 +1,2 @@
+var Odiv=document.getElementById("div1");
+Odiv.innerHTML="hello world1111wozhenshigetiancai";
---
+++ webpack/webpack.config.js
@@ -0,0 +1,19 @@
+const path=require('path');
+module.exports={
+    mode:'development',
+    entry:{
+        'index':'./src/index.js'
+    },
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'
+    },
+    module:{},
+    plugins:[],
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'127.0.0.1',
+        compress:true,
+        port:'8081'
+    }
+};
