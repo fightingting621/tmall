e48b51e1d92c7c3e468f3e21f3a405771e14d083
Dell <qwertxz>
2018-10-12 20:57:29 +0800

    模仿完善美团，运用react和ant design

--- react-example/src/components/Detail.js
+++ react-example/src/components/Detail.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 import DImg from './Detail/DImg';
 import 'whatwg-fetch';
+import Container from './Container';
+import Comment from './Detail/Comment';
 
 export default class Detail extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            row:{},
+            results:[]
+        }
     }
    componentWillMount(){
 
       fetch("http://www.xiechenxi.cn/welcome/detail/"+this.props.match.params.id)
+          .then((response)=>{
+              return response.json();
+          })
+          .then((data)=>{
+              this.setState({
+                  row:data.row,
+                  results:data.results
+              });
+              console.log(data);
+          })
+
    }
 
   render() {
     return (
       <div>
-        <DImg></DImg>
+          <DImg row={this.state.row}></DImg>
+          <Container title="评价">
+              {
+                  this.state.results.map((elem)=>{
+                      return <Comment elem={elem} key={elem.id}></Comment>
+                  })
+              }
+          </Container>
       </div>
     )
   }
